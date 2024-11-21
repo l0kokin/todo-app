2df2bff1406ba13443e871cb3a32d07892507f90
l0kokin <sali.gogishvili1@gmail.com>
2024-11-21 12:49:36 +0400

    chore: delete unused files

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-	return view('home');
-});
+Route::view('/', 'home');
 
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
