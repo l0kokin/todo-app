2cd19c5fa1f65457771cb2ca3e61ccc7efd6af73
l0kokin <sali.gogishvili1@gmail.com>
2024-11-21 16:20:18 +0400

    fix: fix font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Task Manager</title>
-    <link href="https://fonts.googleapis.com/css2?family=Helvetica+Neue:wght@400;700&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css' ])
 
 </head>
