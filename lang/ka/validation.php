4c841490d746ae79336bdd67ac22fe5f87cf2761
l0kokin <sali.gogishvili1@gmail.com>
2024-11-25 18:13:34 +0400

    fix: change validation in georgian

--- lang/ka/validation.php
+++ lang/ka/validation.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-	// 'attributes' => [
-	// 	'name.en'        => 'სახელი',
-	// 	'name.ka'        => 'სახელი',
-	// 	'description.en' => 'აღწერა',
-	// 	'description.ka' => 'აღწერა',
-	// 	'due_date'       => 'საბოლოო თარიღი',
-	// ],
+	'attributes' => [
+		'name.en'        => 'სახელი',
+		'name.ka'        => 'სახელი',
+		'description.en' => 'აღწერა',
+		'description.ka' => 'აღწერა',
+		'due_date'       => 'საბოლოო თარიღი',
+	],
 ];
