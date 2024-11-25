c57b45c04f3d66721bcd3d314e021d46eb13ab05
l0kokin <sali.gogishvili1@gmail.com>
2024-11-25 16:22:02 +0400

    implement posing task functionality for guest users

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,6 @@ Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store')->middleware('guest');
 
 Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('taskInner');
