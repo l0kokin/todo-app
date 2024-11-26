292d44c4b50f3b16161165dab5f6dfe9e892518d
l0kokin <sali.gogishvili1@gmail.com>
2024-11-26 11:36:56 +0400

    style: markup for authorisation page

--- resources/views/components/languages.blade.php
+++ resources/views/components/languages.blade.php
@@ -1,4 +1,9 @@
-<div class="flex gap-x-1 absolute bottom-0 right-0">
+@php
+    $classes = 'flex gap-x-1 absolute bottom-0 right-0'
+@endphp
+
+<div {{ $attributes([ 'class' => $classes ]) }}>
     <p class="text-sm px-4 py-3 rounded-xl hover:bg-gray">English</p>
     <p class="text-sm px-4 py-3 rounded-xl hover:bg-gray">ქართული</p>
 </div>
+
