1d254664eb57a75688aa519a4a68ce46c3f4b32d
l0kokin <sali.gogishvili1@gmail.com>
2024-11-26 15:24:54 +0400

    style: finish markup of authorisation page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,31 +13,33 @@
         {{-- Cover photo --}}
         <img src="{{ asset('images/pattern.png') }}" alt="Cover" class="m-10 ">
 
-        {{-- Sign in --}}
+        {{-- Sign in form --}}
         <div class="flex min-h-full flex-col justify-center mx-20 w-1/2 relative">
             <div class="mt-10 flex justify-between sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 class=" text-left text-3xl font-bold tracking-tight text-black uppercase whitespace-nowrap">@lang('auth.welcome')</h2>
                 <img src="{{ asset('icons/smile.svg') }}" alt="Smiley face" class="">
             </div>
             <form class="space-y-6 mt-10 sm:mx-auto sm:w-full sm:max-w-sm" action="#" method="POST">
+                <x-input-field 
+                    type="email"
+                    name="email"
+                    id="email"
+                    value="email" 
+                    label="auth.email"
+                    error="email"
+                />     
+            
+                <x-input-field 
+                    type="password"
+                    name="password"
+                    id="password"
+                    value="password" 
+                    label="auth.password"
+                    error="password"
+                />     
+
                 <div>
-                    <label for="email" class="block text-sm/6 font-medium text-gray-900">@lang('auth.email')</label>
-                    <div class="mt-2">
-                        <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
-                    </div>
-                </div>    
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label for="password" class="block text-sm/6 font-medium text-gray-900">@lang('auth.password')</label>
-                        <div class="text-sm">
-                        </div>
-                    </div>
-                    <div class="mt-2">
-                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
-                    </div>
-                </div>   
-                <div>
-                    <x-button-white type="submit" class="uppercase bg-blue hover:bg-blue-darker text-white w-full flex justify-center">@lang('auth.welcome')</x-button-white>
+                    <x-button-white type="submit" class="uppercase bg-blue hover:bg-blue-darker text-white w-full flex justify-center">@lang('auth.login')</x-button-white>
                 </div>
             </form>
             <x-languages class="bottom-10 right-40"  />
