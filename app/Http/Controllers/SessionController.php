a3c491f23df049d4e8a5718d84e13e8811ef8ef2
l0kokin <sali.gogishvili1@gmail.com>
2024-11-26 23:22:19 +0400

    fix: session controller

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
-	public function create()
-	{
-		Route::view('/', 'home');
-	}
-
 	public function store(LoginRequest $request)
 	{
 		$attributes = $request->validated();
