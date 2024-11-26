3e4e2124b50ba38abf31fa0912aae0d61350faaf
l0kokin <sali.gogishvili1@gmail.com>
2024-11-26 15:15:40 +0400

    refactor: input fields in create task page for cleaner code and reusability

---
+++ resources/views/components/input-field.blade.php
@@ -0,0 +1,35 @@
+@php
+    $inputType = $inputType ?? 'input';
+@endphp
+
+<div class="relative mt-6">
+    @if($inputType === 'textarea')
+        <textarea 
+            name="{{ $name }}" 
+            id="{{ $id }}" 
+            rows="{{ $rows ?? '' }}" 
+            class="block w-full rounded-lg p-6 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer
+            @error($error) outline-red outline-1 @else border-gray @enderror"
+            {{ $attributes }}
+        >{{ old($value) }}</textarea>
+    @else
+        <input 
+            type="{{ $type ?? 'text' }}" 
+            name="{{ $name }}" 
+            id="{{ $id }}" 
+            value="{{ old($value) }}" 
+            class="block w-full rounded-lg p-6 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer
+            @error($error) outline-red outline-1 @else border-gray @enderror"
+            {{ $attributes }}
+        />
+    @endif
+
+    <label 
+        for="{{ $id }}" 
+        class="absolute left-6 top-2 text-sm text-dark-gray transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:dark-gray peer-focus:top-2 peer-focus:text-sm peer-focus:text-blue-500">
+        {{ __($label) }}
+    </label>
+    @error($error)
+        <p class="mt-2 ml-6 text-xs text-red">{{ $message }}</p>
+    @enderror
+</div>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,104 +6,57 @@
         
         <form method="POST" action="{{ route('tasks.store') }}" class="min-w-[35%] -mt-12">
             @csrf
+
+            <x-input-field 
+                type="text"
+                name="name[en]"
+                id="nameEnglish"
+                value="name.en" 
+                label="create.nameEnglish"
+                error="name.en"
+            />
+
+            <x-input-field 
+                type="text"
+                name="name[ka]"
+                id="nameGeorgian"
+                value="name.ka" 
+                label="create.nameGeorgian"
+                error="name.ka"
+            />
+
+            <x-input-field 
+                inputType="textarea"
+                name="description[en]"
+                id="descriptionEnglish"
+                value="description.en" 
+                rows="3" 
+                label="create.descriptionEnglish"
+                error="description.en"
+            />
             
-            <div class="relative mt-6">
-                <input 
-                    type="text" 
-                    name="name[en]"
-                    id="nameEnglish" 
-                    value="{{ old('name.en') }}"
-                    class="block w-full rounded-lg p-6 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer
-                    @error('name.en') outline-red outline-1 @else border-gray @enderror"
-                />
-                <label 
-                    for="nameEnglish" 
-                    class="absolute left-6 top-2 text-sm text-dark-gray transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:dark-gray peer-focus:top-2 peer-focus:text-sm peer-focus:text-blue-500">
-                    @lang('create.nameEnglish')
-                </label>
-                @error('name.en')
-                    <p class="mt-2 ml-6 text-xs text-red">{{ $message }}</p>
-                @enderror
-            </div>
+            <x-input-field 
+                inputType="textarea"
+                name="description[ka]"
+                id="descriptionGeorgian"
+                value="description.ka" 
+                rows="3" 
+                label="create.descriptionGeorgian"
+                error="description.ka"
+            />
 
-            <div class="relative mt-6">
-                <input 
-                    type="text" 
-                    name="name[ka]"
-                    id="nameGeorgian" 
-                    value="{{ old('name.ka') }}"
-                    class="block w-full rounded-lg p-6 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer
-                    @error('name.ka') outline-red outline-1 @else border-gray-300 @enderror"
-                />
-                <label 
-                    for="nameGeorgian" 
-                    class="absolute left-6 top-2 text-sm text-dark-gray transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:dark-gray peer-focus:top-2 peer-focus:text-sm peer-focus:text-blue-500">
-                    @lang('create.nameGeorgian')
-                </label>
-                @error('name.ka')
-                    <p class="mt-2 ml-6 text-xs text-red">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="relative mt-6">
-                <textarea 
-                    id="descriptionEnglish"
-                    name="description[en]"
-                    rows="3" 
-                    class="block w-full rounded-lg p-6 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer
-                    @error('description.en') outline-red outline-1 @else border-gray-300 @enderror"
-                >{{ old('description.en') }}</textarea>
-                <label 
-                    for="descriptionEnglish" 
-                    class="absolute left-6 top-2 text-sm text-dark-gray transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:dark-gray peer-focus:top-2 peer-focus:text-sm peer-focus:text-blue-500"                >
-                    @lang('create.descriptionEnglish')
-                </label>
-                @error('description.en')
-                    <p class="mt-2 ml-6 text-xs text-red">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="relative mt-6">
-                <textarea 
-                    id="descriptionGeorgian" 
-                    name="description[ka]"
-                    rows="3" 
-                    class="block w-full rounded-lg p-6 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer
-                    @error('description.ka') outline-red outline-1 @else border-gray-300 @enderror"
-                >{{ old('description.ka') }}</textarea>
-                <label 
-                    for="descriptionGeorgian" 
-                    class="absolute left-6 top-2 text-sm text-dark-gray transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:dark-gray peer-focus:top-2 peer-focus:text-sm peer-focus:text-blue-500"                >
-                    @lang('create.descriptionGeorgian')
-                </label>
-                @error('description.ka')
-                    <p class="mt-2 ml-6 text-xs text-red">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="relative mt-6">
-                <input 
-                    type="date" 
-                    name="due_date"
-                    id="date" 
-                    value="{{ old('due_date') }}"
-                    class="block w-full rounded-lg p-6 pt-10 text-black bg-gray focus:ring-2 focus:border-blue sm:text-sm outline-none peer uppercase appearance-none
-                    @error('due_date') outline-red outline-1 @else border-gray-300 @enderror"
-                />
-                <label 
-                    for="date" 
-                    class="absolute left-6 top-4 text-dark-gray text-sm transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-dark-gray peer-focus:top-2 peer-focus:text-sm peer-focus:text-blue-500"
-                >
-                    @lang('create.dueDate')
-                </label>
-                @error('due_date')
-                    <p class="mt-2 ml-6 text-xs text-red">{{ $message }}</p>
-                @enderror
-            </div>     
+            <x-input-field 
+                type="date"
+                name="due_date"
+                id="date"
+                value="due_date" 
+                rows="3" 
+                label="table.dueDate"
+                error="due_date"
+            />
 
             <x-button-white type="submit" class="mt-6 py-8 bg-blue hover:bg-blue-darker text-white w-full flex justify-center">Create Task</x-button-white>       
         </form>
     </div>
-    
     <x-languages/>
 </x-layout>
