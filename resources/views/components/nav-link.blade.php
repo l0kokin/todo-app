5e1edf9a2a66a4d015b87da3ef95aa5b69f53509
l0kokin <sali.gogishvili1@gmail.com>
2024-11-26 22:17:52 +0400

    feat: add logout functionality

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,6 @@
-@props(['icon'])
+@props(['icon', 'href'])
 
-<a href="#" class="block px-3 py-2 text-md font-medium text-text-color mb-4 flex gap-x-2 items-center">
+<a href="{{ $href }}" class="block px-3 py-2 text-md font-medium text-text-color mb-4 flex gap-x-2 items-center">
     <img src="{{ asset('icons/' . $icon . '.svg') }}" alt="Icon" class="w-8 h-8">
     {{ $slot }}
 </a>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@ Route::get('/login', [SessionController::class, 'create'])->name('session.create
 Route::get('/logout', [SessionController::class, 'destroy'])->name('session.destroy');
 
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-
 Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
