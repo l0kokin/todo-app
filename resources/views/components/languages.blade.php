40b553088e9ba38624824d825975a1b1eaeb5ac6
l0kokin <sali.gogishvili1@gmail.com>
2024-12-03 15:35:08 +0400

    style: add background color to active language

--- resources/views/components/languages.blade.php
+++ resources/views/components/languages.blade.php
@@ -1,19 +1,24 @@
 @php
-    $classes = 'flex gap-x-1 absolute bottom-0 right-0'
+    $classes = 'flex gap-x-1 absolute bottom-0 right-0';
+    $currentLocale = app()->getLocale();
 @endphp
 
-<div {{ $attributes([ 'class' => $classes ]) }}>
+<div {{ $attributes(['class' => $classes]) }}>
     <form action="{{ route('session.locale', ['locale' => 'en']) }}" method="post">
         @csrf
         @method('PUT')
 
-        <button class="text-sm px-4 py-3 rounded-xl hover:bg-gray">English</button>
+        <button class="text-sm px-4 py-3 rounded-xl hover:bg-gray {{ $currentLocale === 'en' ? 'bg-gray' : '' }}">
+            English
+        </button>
     </form>
 
     <form action="{{ route('session.locale', ['locale' => 'ka']) }}" method="post">
         @csrf
         @method('PUT')
 
-        <button class="text-sm px-4 py-3 rounded-xl hover:bg-gray">ქართული</button>
+        <button class="text-sm px-4 py-3 rounded-xl hover:bg-gray {{ $currentLocale === 'ka' ? 'bg-gray' : '' }}">
+            ქართული
+        </button>
     </form>
 </div>
