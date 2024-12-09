b59e86be775dcb83bd5aeeee3a0eef16ceff38f6
l0kokin <sali.gogishvili1@gmail.com>
2024-12-09 15:48:42 +0400

    fix: input field width on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                 <img src="{{ asset('icons/smile.svg') }}" alt="Smiley face">
             </div>
 
-            <form class="space-y-6 mt-10 sm:mx-auto sm:w-full sm:max-w-sm" action="/login" method="POST">
+            <form class="space-y-6 mt-10 sm:mx-auto w-[31rem] sm:max-w-sm" action="/login" method="POST">
                 @csrf
                 <x-input-field 
                     type="email"
